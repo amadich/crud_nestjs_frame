df2f55e7dc1cfd3229dccd2bcad508704503e298
amadich <jeridi.moha@gmail.com>
2024-05-26 02:22:18 +0100

    Nest Create Userv:1.3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }));
+  
+  // Enable CORS
+  app.use(cors());
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
+
 bootstrap();
